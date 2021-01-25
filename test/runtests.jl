cc304e76b17fccf5ad95108eafd173a15a70c5ac
Thomas Gillam <tpgillam@googlemail.com>
2021-01-25 23:16:38 +0000

    Add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,12 @@ using Test
 @testset "FixedWindowState" begin
     @testset "empty" begin
         for T in [Float64, Bool, Array{Float64, 1}]
-            state = FixedWindowState{T}(5)
-            @test state.data == T[]
-            @test state.i == 0
+            for capacity in [1, 2, 3]
+                state = FixedWindowState{T}(capacity)
+                @test state.data == T[]
+                @test state.i == 0
+                @test state.capacity == capacity
+            end
         end
     end
 
@@ -17,6 +20,13 @@ using Test
             push!(state, initial)
             @test state.data == [initial]
             @test state.i == 1
+            @test state.capacity == 5
+
+            push!(state, initial, initial)
+            @test state.data == [initial, initial, initial]
+            @test state.i == 3
+            @test state.capacity == 5
         end
     end
+
 end
