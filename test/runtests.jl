14f859d81b87bf30934396e768091f14c8b4faba
Thomas Gillam <tpgillam@googlemail.com>
2021-01-26 22:45:20 +0000

    Refactor tests a bit.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "Rolling" begin
     include("windowed_associative_op.jl")
+    include("fixed_window_associative_op.jl")
 end
--- test/windowed_associative_op.jl
+++ test/windowed_associative_op.jl
@@ -1,70 +1,15 @@
-@testset "empty" begin
-    for T in [Float64, Bool, Array{Float64, 1}]
-        for op in [+, -, *]
-            state = WindowedAssociativeOpState{T}(op)
-            @test state.value_count == 0
-            @test state.op == op
-            @test state.previous_cumsum == T[]
-            @test state.ri_previous_cumsum == 0
-            @test state.values == T[]
-            @test isnothing(state.sum)
-        end
-    end
-end
-
-function test_fixed_window(values, op; approximate_equality::Bool=false)
-    T = typeof(first(values))
-    for emit_early in [true, false]
-        @test_throws ArgumentError FixedWindowAssociativeOp{T}(op, 0; emit_early=emit_early)
-
-        for window in 1:2 * length(values)
-            state = FixedWindowAssociativeOp{T}(op, window; emit_early=emit_early)
-            for (i, value) in enumerate(values)
-                received = update_state!(state, value)
-                if !emit_early && i < window
-                    @test isnothing(received)
-                else
-                    # This is the expected range of the input values over which we are
-                    # reducing.
-                    i_start = max(1, i - window + 1)
-                    i_stop = i
-                    expected = reduce(op, values[i_start:i_stop])
-                    if approximate_equality
-                        @test received ≈ expected
-                    else
-                        @test received == expected
-                    end
-                end
+@testset "WindowedAssociativeOpState" begin
+    @testset "empty" begin
+        for T in [Float64, Bool, Array{Float64, 1}]
+            for op in [+, -, *]
+                state = WindowedAssociativeOpState{T}(op)
+                @test state.value_count == 0
+                @test state.op == op
+                @test state.previous_cumsum == T[]
+                @test state.ri_previous_cumsum == 0
+                @test state.values == T[]
+                @test isnothing(state.sum)
             end
         end
     end
-end
-
-@testset "fixed window trivial" begin
-    T = Int64
-    op = +
-    window = 2
-    state = FixedWindowAssociativeOp{T}(op, window)
-    @test isnothing(update_state!(state, 3))
-    @test update_state!(state, 4) == 7
-    @test update_state!(state, 4) == 8
-    @test update_state!(state, -5) == -1
-end
-
-@testset "fixed window integral" begin
-    for op in (+, *, max, min)
-        test_fixed_window(1:20, op)
-    end
-end
-
-@testset "fixed window random float" begin
-    values = rand(20)
-    for op in (+, *, max, min)
-        test_fixed_window(values, op; approximate_equality=true)
-    end
-end
-
-@testset "fixed window set union" begin
-    values = [Set([x]) for x in 1:20]
-    test_fixed_window(values, union)
 end
