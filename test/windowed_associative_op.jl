e450214a7760906d6f627ad4f9c53d01887daf24
Thomas Gillam <tpgillam@googlemail.com>
2021-01-26 22:13:39 +0000

    Add some more tests.

--- test/windowed_associative_op.jl
+++ test/windowed_associative_op.jl
@@ -12,7 +12,35 @@
     end
 end
 
-@testset "fixed window" begin
+function test_fixed_window(values, op; approximate_equality::Bool=false)
+    T = typeof(first(values))
+    for emit_early in [true, false]
+        @test_throws ArgumentError FixedWindowAssociativeOp{T}(op, 0; emit_early=emit_early)
+
+        for window in 1:2 * length(values)
+            state = FixedWindowAssociativeOp{T}(op, window; emit_early=emit_early)
+            for (i, value) in enumerate(values)
+                received = update_state!(state, value)
+                if !emit_early && i < window
+                    @test isnothing(received)
+                else
+                    # This is the expected range of the input values over which we are
+                    # reducing.
+                    i_start = max(1, i - window + 1)
+                    i_stop = i
+                    expected = reduce(op, values[i_start:i_stop])
+                    if approximate_equality
+                        @test received ≈ expected
+                    else
+                        @test received == expected
+                    end
+                end
+            end
+        end
+    end
+end
+
+@testset "fixed window trivial" begin
     T = Int64
     op = +
     window = 2
@@ -21,4 +49,17 @@ end
     @test update_state!(state, 4) == 7
     @test update_state!(state, 4) == 8
     @test update_state!(state, -5) == -1
+end
+
+@testset "fixed window integral" begin
+    for op in (+, *, max, min)
+        test_fixed_window(1:20, op)
+    end
+end
+
+@testset "fixed window random float" begin
+    values = rand(20)
+    for op in (+, *, max, min)
+        test_fixed_window(values, op; approximate_equality=true)
+    end
 end
