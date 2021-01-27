438d77af391686f1635c667bdfa1e62f60d64e57
Thomas Gillam <tpgillam@googlemail.com>
2021-01-27 22:03:52 +0000

    Hopefully include package properly now

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,10 @@
 using Documenter, Rolling
 
-makedocs(sitename="Rolling.jl")
+makedocs(
+    modules=[Rolling],
+    sitename="Rolling.jl"
+)
+
 deploydocs(
     repo="github.com/tpgillam/Rolling.jl.git",
     devbranch="main"
