eb98585df63d1a7699e0ebf794839624010808c6
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-10-21 11:25:06 +0100

    Remove special-casing from CI configuration

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,9 @@ using Documenter, AssociativeWindowAggregation
 # This is inspired by the following, to prevent a `gksqt` process blocking (and needing
 # manual termination) for every plot when generating documentation on MacOS.
 # https://discourse.julialang.org/t/deactivate-plot-display-to-avoid-need-for-x-server/19359/2
+# Another choice could be "100":
+# https://discourse.julialang.org/t/generation-of-documentation-fails-qt-qpa-xcb-could-not-connect-to-display/60988
+# Empirically, both choices seem to work.
 withenv("GKSwstype" => "nul") do
     makedocs(
         format=Documenter.HTML(
