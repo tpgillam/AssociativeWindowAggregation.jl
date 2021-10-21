ccc45958e006378278efac581d58598feedb80dd
Tom Gillam <thomas.gillam@invenialabs.co.uk>
2021-10-21 11:13:11 +0100

    Fix plot generation when building locally

--- docs/make.jl
+++ docs/make.jl
@@ -1,21 +1,26 @@
 using Documenter, AssociativeWindowAggregation
 
-makedocs(
-    format=Documenter.HTML(
-        prettyurls=get(ENV, "CI", "false") == "true"
-    ),
-    modules=[AssociativeWindowAggregation],
-    sitename="AssociativeWindowAggregation.jl",
-    pages=[
-        "Home" => "index.md",
-        "examples.md",
-        "Reference" => [
-            "General window" => "reference/windowed_associative_op.md",
-            "Fixed window" => "reference/fixed_window_associative_op.md",
-            "Time window" => "reference/time_window_associative_op.md",
+# This is inspired by the following, to prevent a `gksqt` process blocking (and needing
+# manual termination) for every plot when generating documentation on MacOS.
+# https://discourse.julialang.org/t/deactivate-plot-display-to-avoid-need-for-x-server/19359/2
+withenv("GKSwstype" => "nul") do
+    makedocs(
+        format=Documenter.HTML(
+            prettyurls=get(ENV, "CI", "false") == "true"
+        ),
+        modules=[AssociativeWindowAggregation],
+        sitename="AssociativeWindowAggregation.jl",
+        pages=[
+            "Home" => "index.md",
+            "examples.md",
+            "Reference" => [
+                "General window" => "reference/base.md",
+                "Fixed window" => "reference/fixed.md",
+                "Time window" => "reference/time.md",
+            ]
         ]
-    ]
-)
+    )
+end
 
 deploydocs(
     repo="github.com/tpgillam/AssociativeWindowAggregation.jl.git",
